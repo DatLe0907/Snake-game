66a991756b04fbf922eaf5407688793c6a15dec5
DatLe <letandat9783@gmail.com>
2023-08-08 17:07:49 +0700

    snake v1.2 update sound and add new object-rock

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,13 @@ const scoreModal = document.querySelector('.score');
 const highScoreModal = document.querySelector('.high-score');
 const replayBtn = document.querySelector('.modal__content button');
 const controlBtn = document.querySelectorAll('.control button');
+const audioEatingApple = new Audio('./assets/audio/biting-into-an-apple.mp3');
+const audioHurt = new Audio('./assets/audio/umph.mp3');
+const themeSong = new Audio('./assets/audio/Snake Game - Theme Song.mp3');
 
+document.addEventListener('onloadend',function(e){
+    themeSong.play()
+})
 
 
 let snakeX = 15, snakeY = 15;
@@ -13,6 +19,22 @@ let randomArr = [1,-1]
 let foodX = snakeX + randomArr[Math.floor(Math.random() * randomArr.length)],
 foodY = snakeY + randomArr[Math.floor(Math.random() * randomArr.length)];
 let velocityX = 0, velocityY = 0;
+let rockX,rockY;
+let rockList = []
+function randomRock(){
+    for(let i =0; i<4;i++){
+        rockX = Math.floor(Math.random() * 30 + 1);
+        rockY = Math.floor(Math.random() * 30 + 1);
+        rockList.push([rockX,rockY]);
+        console.log(rockList)
+        if((rockX === snakeX && rockY === snakeY)|| (rockX === foodX && rockY === foodY)){
+        randomRock()
+    }
+    }
+}
+
+randomRock();
+
 let speed = 100;
 let snakeBody = [];
 let gameOver = false;
@@ -27,6 +49,9 @@ let highScore = localStorage.getItem("heading__high-score") || 0;
 const randomFoodPosition = ()=> {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
+    if((foodX === snakeX && foodY === snakeY)|| (foodX === foodX && foodY === foodY)){
+        randomFoodPosition()
+    }
 }
 
 const handleGameOver = () => {
@@ -48,12 +73,18 @@ const initGame = () => {
     if(gameOver){
         handleGameOver();
     }
-    let htmlMarkup = `<div class="game__apple" style = 'grid-area: ${foodY} / ${foodX}'><div class = 'apple'></div></div>`
+    let htmlMarkup = `
+        <div class="game__apple" style = 'grid-area: ${foodY} / ${foodX}'><div class = 'apple'></div></div>
+    `
+    htmlMarkup+=rockList.map(function(rock){
+       return ` <div class = 'rock' style = 'grid-area: ${rock[1]} / ${rock[0]}'></div>`
+    })
     
 
 
 
     if(snakeX === foodX && snakeY === foodY){
+        audioEatingApple.play();
         randomFoodPosition();
         snakeBody.push([foodX,foodY]);
 
@@ -83,14 +114,26 @@ const initGame = () => {
 
     for(let i = 0; i < snakeBody.length; i++){
         htmlMarkup += `<div class="game__snake" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]};"></div>`;
-        if(i != 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+        if(i != 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){      
+    audioHurt.play();
+            
             gameOver = true;
         }
     }
 
     if(snakeX < 1 || snakeX > 30 || snakeY < 1 || snakeY > 30){
+        
+        audioHurt.play();
+    
         gameOver = true;
     }
+    rockList.forEach(function(rock){
+        console.log(rock[1], rock[0])
+        if(snakeX === rock[0] && snakeY === rock[1]){
+            audioHurt.play();
+            gameOver = true;
+        }
+    })
 
     boxGame.innerHTML = htmlMarkup;
     let snakeLength = document.querySelectorAll('.game__snake');
