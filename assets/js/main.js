e352c88ca9b48e157229d4a5ba7a46d5ebe8a390
DatLe <letandat9783@gmail.com>
2023-08-09 10:04:11 +0700

    snake v1.3 update replay

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,74 +28,67 @@ let highScore = localStorage.getItem("heading__high-score") || 0;
 highScoreElement.innerText = `High Score: ${highScore}`;
 highScoreModal.innerText = `High Score: ${highScore}`;
 
-// Creat random rock position
-function randomRock() {
-    for (let i = 0; i < 10; i++) {
-        rockX = Math.floor(Math.random() * 30 + 1);
-        rockY = Math.floor(Math.random() * 30 + 1);
-        rockList.push([rockX, rockY]);
-    }
-}
-randomRock();
+let intervalId;
 
-// Game over
-const handleGameOver = () => {
-  clearInterval(setInterValId);
-  modal.classList.add("show");
-  replayBtn.addEventListener("click", function (e) {
-    location.reload();
-  });
-  if (modal.classList.contains("show")) {
-    document.addEventListener("keydown", function (e) {
-      if (e.key === "Enter") {
-        location.reload();
-      }
-    });
+const reset = () => {
+  snakeY = 15;
+  randomArr = [1, -1];
+  foodX = 0;
+  foodY = 0;
+  velocityX = 0;
+  velocityY = 0;
+  rockList = [];
+  rockX = 0;
+  rockY = 0;
+  speed = 100;
+  snakeBody = [];
+  gameOver = false;
+  score = 0;
+};
+
+// Creat random rock position
+const randomRock = () => {
+  for (let i = 0; i < 10; i++) {
+    rockX = Math.floor(Math.random() * 30 + 1);
+    rockY = Math.floor(Math.random() * 30 + 1);
+    rockList.push([rockX, rockY]);
   }
 };
-
-
 // creat random food if food position != rock position else recreate
 const randomFoodPosition = () => {
-    foodX = Math.floor(Math.random() * 30) + 1;
-    foodY = Math.floor(Math.random() * 30) + 1;
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
 };
-
-randomFoodPosition();
-
 const initGame = () => {
-    if (gameOver) {
-        handleGameOver();
-    }
-    let htmlMarkup;
-    rockList.forEach((rock) => {
-        if ((foodX !== rock[0], foodY !== rock[1])) {
-            htmlMarkup = `<div class="game__apple" style = 'grid-area: ${foodY} / ${foodX}'><div class = 'apple'></div></div>`;
-        } 
-        else {
+  if (gameOver) {
+    handleGameOver();
+  }
+  let htmlMarkup;
+  rockList.forEach((rock) => {
+    if ((foodX !== rock[0], foodY !== rock[1])) {
+      htmlMarkup = `<div class="game__apple" style = 'grid-area: ${foodY} / ${foodX}'><div class = 'apple'></div></div>`;
+    } else {
       randomFoodPosition();
     }
-});
-//   create rock
-rockList.forEach(function (rock) {
+  });
+  //   create rock
+  rockList.forEach(function (rock) {
     htmlMarkup += ` <div class = 'rock' style = 'grid-area: ${rock[1]} / ${rock[0]}'></div>`;
-    
-});
+  });
 
-//   if food has eaten snake length + 1
+  //   if food has eaten -> snake length + 1
   if (snakeX === foodX && snakeY === foodY) {
     audioEatingApple.play();
     randomFoodPosition();
     snakeBody.push([foodX, foodY]);
-    console.log(snakeBody)
-
     score++;
+
     // while score % 10 == 0 -> snake speed up
-    if (score % 10 === 0) {
+    if (score % 10 === 0 && score !== 0) {
       speed -= 10;
     }
 
-    // while high score > score -> high score = score; 
+    // while high score > score -> high score = score;
     highScore = score > highScore ? score : highScore;
 
     localStorage.setItem("heading__high-score", highScore);
@@ -105,7 +98,7 @@ rockList.forEach(function (rock) {
     highScoreModal.innerText = `High Score: ${highScore}`;
   }
 
-//   if snake reach rock -> game over
+  //   if snake reach rock -> game over
   rockList.forEach(function (rock) {
     if (snakeX === rock[0] && snakeY === rock[1]) {
       audioHurt.play();
@@ -117,14 +110,14 @@ rockList.forEach(function (rock) {
   snakeX = snakeX + velocityX;
   snakeY = snakeY + velocityY;
 
-//   shift snake body
+  //   shift snake body
   for (let i = snakeBody.length - 1; i >= 0; i--) {
     snakeBody[i] = snakeBody[i - 1];
   }
   snakeBody[0] = [snakeX, snakeY];
 
   for (let i = 0; i < snakeBody.length; i++) {
-        // Adding a div for each part of the snake's body
+    // Adding a div for each part of the snake's body
     htmlMarkup += `<div class="game__snake" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]};"></div>`;
     // if snake head reach snake body -> game over
     if (
@@ -133,12 +126,11 @@ rockList.forEach(function (rock) {
       snakeBody[0][0] === snakeBody[i][0]
     ) {
       audioHurt.play();
-
       gameOver = true;
     }
   }
 
-//   if snake reach wall -> game over
+  //   if snake reach wall -> game over
   if (snakeX < 1 || snakeX > 30 || snakeY < 1 || snakeY > 30) {
     audioHurt.play();
 
@@ -152,40 +144,73 @@ rockList.forEach(function (rock) {
     snakeLength[0].style.background = "#ffff00";
   }
 };
+// random rock and food
+function random() {
+  randomRock();
+  randomFoodPosition();
+}
 
-let setInterValId = setInterval(initGame, speed);
-
-document.addEventListener("keydown", function (e) {
-  // change velocity when key down
-  if (e.key === "ArrowUp" && velocityY != 1) {
-    velocityX = 0;
-    velocityY = -1;
-  } else if (e.key === "ArrowDown" && velocityY != -1) {
-    velocityX = 0;
-    velocityY = 1;
-  } else if (e.key === "ArrowLeft" && velocityX != 1) {
-    velocityX = -1;
-    velocityY = 0;
-  } else if (e.key === "ArrowRight" && velocityX != -1) {
-    velocityX = 1;
-    velocityY = 0;
-  }
-});
-
-controlBtn.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    if (this.classList[0] === "up" && velocityY != 1) {
+function control() {
+  document.addEventListener("keydown", function (e) {
+    // change velocity when key down
+    if (e.key === "ArrowUp" && velocityY != 1) {
       velocityX = 0;
       velocityY = -1;
-    } else if (this.classList[0] === "down" && velocityY != -1) {
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
       velocityX = 0;
       velocityY = 1;
-    } else if (this.classList[0] === "left" && velocityX != 1) {
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
       velocityX = -1;
       velocityY = 0;
-    } else if (this.classList[0] === "right" && velocityX != -1) {
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
       velocityX = 1;
       velocityY = 0;
     }
   });
-});
+
+  controlBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      if (this.classList[0] === "up" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+      } else if (this.classList[0] === "down" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+      } else if (this.classList[0] === "left" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+      } else if (this.classList[0] === "right" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+      }
+    });
+  });
+}
+
+function startGame() {
+  random();
+  control();
+  intervalId = setInterval(initGame, speed);
+}
+
+startGame();
+
+// Game over
+const handleGameOver = () => {
+  clearInterval(intervalId);
+  modal.classList.add("show");
+  replayBtn.addEventListener("click", function (e) {
+    reset();
+    startGame();
+    modal.classList.remove("show");
+  });
+  if (modal.classList.contains("show")) {
+    document.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {
+        reset();
+        startGame();
+        modal.classList.remove("show");
+      }
+    });
+  }
+};
